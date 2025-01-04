1aecd311c7be89d2bdf92b60914954131b0bff92
yuta <y.suzuki336@gmail.com>
2025-01-05 08:50:58 +0900

    login画面のStyling

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -8,12 +8,12 @@ import Header from "../../components/Header";
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
 
-const Edit = (): JSX.Element => {
+const Create = (): JSX.Element => {
   return (
     <KeyboardAvoidingView behavior="height" style={styles.container}>
       <Header />
       <View style={styles.inputcontainer}>
-        <TextInput multiline style={styles.input} value={"買い物\nリスト"} />
+        <TextInput multiline style={styles.input} value="" />
       </View>
       <CircleButton>
         <Icon name="check1" size={40} color="#ffffff" />
@@ -38,4 +38,4 @@ const styles = StyleSheet.create({
     lineHeight: 24,
   },
 });
-export default Edit;
+export default Create;
